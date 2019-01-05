d98c51bb14a11ad7d49f8e87075a049f7e62a400
최인성 <choiis1207@gmail.com>
2019-01-05 19:43:00 +0900

    20190105 Iocp 채팅 대기실 채팅방 입장 기능 구현

--- Chat/Client.cpp
+++ Chat/Client.cpp
@@ -5,7 +5,6 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,10 +12,21 @@
 #include <winsock2.h>
 #include <process.h>
 
-#define BUF_SIZE 100
+#define BUF_SIZE 512
 #define NAME_SIZE 20
 
-#define STATUS_CHAT 2
+// 클라이언트 상태 정보
+#define STATUS_LOGOUT 1
+#define STATUS_WAITING 2
+#define STATUS_CHATTIG 3
+
+// 서버에게 지시 사항
+#define ROOM_MAKE 1
+#define ROOM_ENTER 2
+#define ROOM_OUT 3
+
+// 현재 클라이언트 상태 => 대기실 => 추후 로그인 이전으로 바뀔것
+int clientStatus = 2;
 
 char name[NAME_SIZE] = "";
 char msg[BUF_SIZE];
@@ -28,12 +38,15 @@ typedef struct { // socket info
 	SOCKADDR_IN clntAdr;
 } PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
 
+// 실제 통신 패킷 데이터
 typedef struct {
-	int status;
-	char name[BUF_SIZE];
+	int clientStatus;
+	char name[NAME_SIZE];
 	char message[BUF_SIZE];
+	int direction;
 } PACKET_DATA, *P_PACKET_DATA;
 
+// 비동기 통신에 필요한 구조체
 typedef struct { // buffer info
 	OVERLAPPED overlapped;
 	WSABUF wsaBuf;
@@ -42,22 +55,24 @@ typedef struct { // buffer info
 	int clientMode;
 } PER_IO_DATA, *LPPER_IO_DATA;
 
+// 송신을 담당할 스레드
 unsigned WINAPI SendMsg(void *arg) {
 	SOCKET hSock = *((SOCKET*) arg);
-	char nameMsg[BUF_SIZE];
+	char msg[BUF_SIZE];
 
 	while (1) {
-		cin >> msg;
-		//fgets(msg, BUF_SIZE, stdin);
-
-		if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")
-				|| !strcmp(msg, "out\n")) {
+		gets(msg);
+		if (strcmp(msg, "\n") == 0) { // 입력 안하면 반응 없음
+			continue;
+		} else if (clientStatus == STATUS_WAITING // 대기실
+				&& (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")
+						|| !strcmp(msg, "out\n"))) {
 			// 채팅 끝남 메시지
 			send(hSock, "out", sizeof("out"), 0);
 			closesocket(hSock);
 			exit(1);
 		}
-		sprintf(nameMsg, "%s : %s", name, msg);
+
 		PER_IO_DATA dataBuf;
 		WSAOVERLAPPED overlapped;
 
@@ -66,9 +81,22 @@ unsigned WINAPI SendMsg(void *arg) {
 		overlapped.hEvent = event;
 
 		P_PACKET_DATA packet;
-		packet = (P_PACKET_DATA) malloc(sizeof(PACKET_DATA));
-		strcpy(packet->name, nameMsg);
-		strcpy(packet->message, "채팅중 접속 모드");
+		packet = new PACKET_DATA;
+		if (clientStatus == STATUS_WAITING) { // 대기실 일 때
+			if (strcmp(msg, "2") == 0) {
+				puts("방 이름을 입력해 주세요");
+				gets(msg);
+				packet->direction = ROOM_MAKE;
+			} else if (strcmp(msg, "3") == 0) {
+				puts("입장할 방 이름을 입력해 주세요");
+				gets(msg);
+				packet->direction = ROOM_ENTER;
+			}
+		} else if (clientStatus == STATUS_CHATTIG) { // 채팅중일 때
+
+		}
+		strcpy(packet->name, name);
+		strcpy(packet->message, msg);
 
 		dataBuf.wsaBuf.buf = (char*) packet;
 		dataBuf.wsaBuf.len = sizeof(PACKET_DATA);
@@ -77,6 +105,8 @@ unsigned WINAPI SendMsg(void *arg) {
 	}
 	return 0;
 }
+
+// 수신을 담당할 스레드
 unsigned WINAPI RecvMsg(LPVOID hComPort) {
 
 	SOCKET sock;
@@ -89,12 +119,18 @@ unsigned WINAPI RecvMsg(LPVOID hComPort) {
 		GetQueuedCompletionStatus(hComPort, &bytesTrans, (LPDWORD) &handleInfo,
 				(LPOVERLAPPED*) &ioInfo, INFINITE);
 
-		P_PACKET_DATA packet = (P_PACKET_DATA) malloc(sizeof(PACKET_DATA));
+		P_PACKET_DATA packet = new PACKET_DATA;
 		memcpy(packet, ioInfo->buffer, sizeof(PACKET_DATA));
 
-		strcpy(nameMsg, packet->message);
-
-		if (packet->status == STATUS_CHAT) {
+		// Client의 상태 정보 갱신 필수
+		// 서버에서 준것으로 갱신
+		if (packet->clientStatus == STATUS_WAITING) {
+			clientStatus = packet->clientStatus;
+			strcpy(nameMsg, packet->message);
+			cout << nameMsg << endl;
+		} else if (packet->clientStatus == STATUS_CHATTIG) {
+			clientStatus = packet->clientStatus;
+			strcpy(nameMsg, packet->message);
 			cout << nameMsg << endl;
 		}
 
@@ -104,7 +140,7 @@ unsigned WINAPI RecvMsg(LPVOID hComPort) {
 		}
 
 		int recvBytes, flags = 0;
-		ioInfo = (LPPER_IO_DATA) malloc(sizeof(PER_IO_DATA));
+		ioInfo = new PER_IO_DATA;
 		ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
 		ioInfo->wsaBuf.buf = ioInfo->buffer;
 		WSARecv(sock, &(ioInfo->wsaBuf), 1, (LPDWORD) &recvBytes,
@@ -122,19 +158,16 @@ int main(int argc, char* argv[0]) {
 	LPPER_HANDLE_DATA handleInfo;
 	HANDLE sendThread, recvThread;
 
-//	if (argc != 4) {
-//		cout << "Usage :" << argv[0] << "<port>" << endl;
-//		exit(1);
-//	}
-
 	// Socket lib의 초기화
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
 		printf("WSAStartup() error!");
 		exit(1);
 	}
-
+	// Overlapped IO가능 소켓을 만든다
+	// TCP 통신할것
 	hSocket = WSASocketW(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0,
 	WSA_FLAG_OVERLAPPED);
+
 	memset(&servAddr, 0, sizeof(servAddr));
 	servAddr.sin_family = PF_INET;
 	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -148,7 +181,6 @@ int main(int argc, char* argv[0]) {
 		if (connect(hSocket, (SOCKADDR*) &servAddr,
 				sizeof(servAddr))==SOCKET_ERROR) {
 			printf("connect() error!");
-			exit(1);
 		} else {
 			break;
 		}
@@ -158,33 +190,38 @@ int main(int argc, char* argv[0]) {
 	string user;
 	cin >> user;
 	sprintf(name, "%s", user.c_str());
+
+	// Completion Port 생성
 	HANDLE hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 	PER_IO_DATA dataBuf;
 	WSAOVERLAPPED overlapped;
 
 	WSAEVENT event = WSACreateEvent();
-	handleInfo = (LPPER_HANDLE_DATA) malloc(sizeof(PER_IO_DATA));
+	handleInfo = new PER_HANDLE_DATA;
 	handleInfo->hClntSock = hSocket;
 	int addrLen = sizeof(servAddr);
 	memcpy(&(handleInfo->clntAdr), &servAddr, addrLen);
 	memset(&overlapped, 0, sizeof(OVERLAPPED));
 
+	// Completion Port 와 소켓 연결
 	CreateIoCompletionPort((HANDLE) hSocket, hComPort, (DWORD) handleInfo, 0);
 
 	overlapped.hEvent = event;
 	P_PACKET_DATA packet;
-	packet = (P_PACKET_DATA) malloc(sizeof(PACKET_DATA));
+	packet = new PACKET_DATA;
 	strcpy(packet->name, name);
-	strcpy(packet->message, "최초 접속 모드");
 
 	dataBuf.wsaBuf.buf = (char*) packet;
 	dataBuf.wsaBuf.len = sizeof(PACKET_DATA);
 
+	// 이름정보 먼저 전달
 	WSASend(hSocket, &(dataBuf.wsaBuf), 1, NULL, 0, &overlapped, NULL);
 
-	free(packet);
+	delete packet;
+	// 수신 스레드 동작
 	recvThread = (HANDLE) _beginthreadex(NULL, 0, RecvMsg, (LPVOID) hComPort, 0,
 	NULL);
+	// 송신 스레드 동작
 	sendThread = (HANDLE) _beginthreadex(NULL, 0, SendMsg, (void*) &hSocket, 0,
 	NULL);
 
--- Chat/Server.cpp
+++ Chat/Server.cpp
@@ -1,11 +1,10 @@
 //============================================================================
-// Name        : IocpServer.cpp
+// Name        : Server.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-// https://dramadramingdays.tistory.com/119
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,31 +12,44 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <map>
-#include <vector>
+#include <list>
 
 using namespace std;
 
-#define BUF_SIZE 100
+#define BUF_SIZE 512
+#define NAME_SIZE 20
+
 #define START 1
 #define READ 2
 #define WRITE 3
 
-#define STATUS_CHAT 2
-int const NAME_SIZE = 10;
+// 클라이언트 상태 정보 => 서버에서 보관할것
+#define STATUS_LOGOUT 1
+#define STATUS_WAITING 2
+#define STATUS_CHATTIG 3
 
+// 서버에 올 지시 사항
+#define ROOM_MAKE 1
+#define ROOM_ENTER 2
+#define ROOM_OUT 3
+
+// 서버에 접속한 유저 정보
 typedef struct { // socket info
 	SOCKET hClntSock;
 	SOCKADDR_IN clntAdr;
-	string roomName;
+	char roomName[NAME_SIZE];
 	int status;
 } PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
 
+// 실제 통신 패킷 데이터
 typedef struct {
-	int status;
-	char name[BUF_SIZE];
+	int clientStatus;
+	char name[NAME_SIZE];
 	char message[BUF_SIZE];
+	int direction;
 } PACKET_DATA, *P_PACKET_DATA;
 
+// 비동기 통신에 필요한 구조체
 typedef struct { // buffer info
 	OVERLAPPED overlapped;
 	WSABUF wsaBuf;
@@ -45,92 +57,170 @@ typedef struct { // buffer info
 	int serverMode;
 } PER_IO_DATA, *LPPER_IO_DATA;
 
-typedef struct {
-	char id[NAME_SIZE];
-	unsigned int level;
-} USER_INFO, *PUSER_INFO;
-
+// 서버에 접속한 유저 자료 저장
 map<string, LPPER_HANDLE_DATA> userMap;
-map<string, vector<string> > roomMap;
+// 서버의 방 정보 저장
+map<string, list<string> > roomMap;
 
+// 임계영역에 필요한 오브젝트
+// WaitForSingleObject는 mutex를 non-singled로 만듬 => 시작
+// ReleaseMutex는  mutex를 singled로 만듬 => 끝
 HANDLE mutex;
 
-void SendToAllMsg(LPPER_IO_DATA ioInfo, char *msg, SOCKET mySock) {
+// 본인에게 메세지 전달
+void SendToMeMsg(LPPER_IO_DATA ioInfo, char *msg, SOCKET mySock, int status) {
 
-	printf("SendToAllMsg %s\n", msg);
-	ioInfo = (LPPER_IO_DATA) malloc(sizeof(PER_IO_DATA));
+	cout << "SendToOneMsg " << msg << endl;
+
+	ioInfo = new PER_IO_DATA;
+
 	memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
-	P_PACKET_DATA packet;
-	packet = (P_PACKET_DATA) malloc(sizeof(PACKET_DATA));
+	P_PACKET_DATA packet = new PACKET_DATA;
+
 	strcpy(packet->message, msg);
-	packet->status = STATUS_CHAT;
+	packet->clientStatus = status;
 	ioInfo->wsaBuf.buf = (char*) packet;
 	ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
 	ioInfo->serverMode = WRITE;
-	// WaitForSingleObject(mutex, INFINITE);
-	map<string, LPPER_HANDLE_DATA>::iterator iter;
-	for (iter = userMap.begin(); iter != userMap.end(); iter++) {
-		//if ((*iter->second).hClntSock == mySock) {
-		//	continue;
-		//} else {
-		WSASend((*iter->second).hClntSock, &(ioInfo->wsaBuf), 1, NULL, 0,
-				&(ioInfo->overlapped), NULL);
-		//}
-	}
-	// ReleaseMutex(mutex);
+
+	WSASend(mySock, &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+
+	delete ioInfo;
+	// delete packet;
 }
 
+// 같은 방의 사람들에게 메세지 전달
+void SendToRoomMsg(LPPER_IO_DATA ioInfo, char *msg, list<string> lists,
+		int status) {
+
+	cout << "SendToRoomMsg " << msg << endl;
+
+	ioInfo = new PER_IO_DATA;
+	memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+	P_PACKET_DATA packet = new PACKET_DATA;
+
+	strcpy(packet->message, msg);
+	packet->clientStatus = status;
+	ioInfo->wsaBuf.buf = (char*) packet;
+	ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
+	ioInfo->serverMode = WRITE;
+
+	WaitForSingleObject(mutex, INFINITE);
+	list<string>::iterator iter;
+	for (iter = lists.begin(); iter != lists.end(); iter++) {
+		WSASend(userMap.find(*iter)->second->hClntSock, &(ioInfo->wsaBuf), 1,
+		NULL, 0, &(ioInfo->overlapped), NULL);
+	}
+	ReleaseMutex(mutex);
+
+	delete ioInfo;
+	// delete packet;
+}
+
+// 전체 사람들에게 메세지 전달
+// 사자후 기능 추후 구현 예정
+void SendToAllMsg(LPPER_IO_DATA ioInfo, char *msg, SOCKET mySock, int status) {
+
+	cout << "SendToAllMsg " << msg << endl;
+
+	ioInfo = new PER_IO_DATA;
+	memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+	P_PACKET_DATA packet = new PACKET_DATA;
+
+	strcpy(packet->message, msg);
+	packet->clientStatus = status;
+	ioInfo->wsaBuf.buf = (char*) packet;
+	ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
+	ioInfo->serverMode = WRITE;
+
+	WaitForSingleObject(mutex, INFINITE);
+	map<string, LPPER_HANDLE_DATA>::iterator iter;
+	for (iter = userMap.begin(); iter != userMap.end(); iter++) {
+		if ((*iter->second).hClntSock == mySock) { // 본인 제외
+			continue;
+		} else {
+			WSASend((*iter->second).hClntSock, &(ioInfo->wsaBuf), 1, NULL, 0,
+					&(ioInfo->overlapped), NULL);
+		}
+	}
+	ReleaseMutex(mutex);
+
+	// delete packet;
+	delete ioInfo;
+}
+
+// 초기 접속
+void InitUser(P_PACKET_DATA packet, LPPER_IO_DATA ioInfo, SOCKET sock,
+		LPPER_HANDLE_DATA handleInfo) {
+
+	char msg[BUF_SIZE];
+	char name[NAME_SIZE];
+	DWORD flags = 0;
+	packet = new PACKET_DATA;
+
+	memcpy(packet, ioInfo->buffer, sizeof(PACKET_DATA));
+	strcpy(name, packet->name);
+
+	cout << "START user : " << name << endl;
+	cout << "sock : " << handleInfo->hClntSock << endl;
+	// 유저의 상태 정보 초기화
+	handleInfo->status = STATUS_WAITING;
+	strcpy(handleInfo->roomName, "");
+	WaitForSingleObject(mutex, INFINITE);
+	userMap.insert(pair<string, LPPER_HANDLE_DATA>(name, handleInfo));
+	cout << "현재 접속 인원 수 : " << userMap.size() << endl;
+	ReleaseMutex(mutex);
+
+	delete ioInfo;
+
+	strcpy(msg, "입장을 환영합니다!\n");
+	strcat(msg, "1.방 정보 보기, 2.방 만들기 3.방 입장하기");
+	SendToMeMsg(ioInfo, msg, sock, STATUS_WAITING);
+
+	ioInfo = new PER_IO_DATA;
+
+	memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
+	ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
+	ioInfo->wsaBuf.buf = ioInfo->buffer;
+	ioInfo->serverMode = READ;
+
+	WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags, &(ioInfo->overlapped),
+	NULL);
+
+}
+
+// Server 컴퓨터  CPU 개수만큼 스레드 생성될것
 unsigned WINAPI HandleThread(LPVOID pCompPort) {
+
 	HANDLE hComPort = (HANDLE) pCompPort;
 	SOCKET sock;
 	DWORD bytesTrans;
 	LPPER_HANDLE_DATA handleInfo;
 	LPPER_IO_DATA ioInfo;
-	DWORD flags = 0;
-	char msg[BUF_SIZE];
-	char mode[BUF_SIZE];
+
 	P_PACKET_DATA packet;
 
 	while (true) {
+
 		GetQueuedCompletionStatus(hComPort, &bytesTrans, (LPDWORD) &handleInfo,
 				(LPOVERLAPPED*) &ioInfo, INFINITE);
+		// IO 완료후 동작 부분
 		sock = handleInfo->hClntSock;
 
 		if (START == ioInfo->serverMode) {
 			cout << "START" << endl;
-			packet = (P_PACKET_DATA) malloc(sizeof(PACKET_DATA));
-			memcpy(packet, ioInfo->buffer, sizeof(PACKET_DATA));
-			strcpy(msg, packet->name);
-			strcpy(mode, packet->message);
-
-			// WaitForSingleObject(mutex, INFINITE);
-
-			cout << "START message : " << msg << endl;
-			cout << "clientMode : " << mode << endl;
-
-			userMap.insert(pair<string, LPPER_HANDLE_DATA>(msg, handleInfo));
-			cout << "현재 접속 인원 수 : " << userMap.size() << endl;
-
-			// ReleaseMutex(mutex);
-
-			free(ioInfo);
-			strcat(msg, " 님이 입장하셨습니다\n");
-			SendToAllMsg(ioInfo, msg, sock);
-
-			ioInfo = (LPPER_IO_DATA) malloc(sizeof(PER_IO_DATA));
-			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
-			ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
-			ioInfo->wsaBuf.buf = ioInfo->buffer;
-			ioInfo->serverMode = READ;
-
-			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags,
-					&(ioInfo->overlapped), NULL);
+			InitUser(packet, ioInfo, sock, handleInfo);
 
 		} else if (READ == ioInfo->serverMode) {
-			puts("message received");
+			cout << "message received" << endl;
 
-			if (bytesTrans == 0 || strcmp(ioInfo->buffer, "out") == 0) {
-				// WaitForSingleObject(mutex, INFINITE);
+			char name[BUF_SIZE];
+			char msg[BUF_SIZE];
+			DWORD flags = 0;
+			int direction = 0;
+
+			if (bytesTrans == 0 || strcmp(ioInfo->buffer, "out") == 0) { // 접속 끊김
+				WaitForSingleObject(mutex, INFINITE);
 				map<string, LPPER_HANDLE_DATA>::iterator iter;
 				for (iter = userMap.begin(); iter != userMap.end(); iter++) {
 					if (sock == (*iter).second->hClntSock) {
@@ -139,36 +229,157 @@ unsigned WINAPI HandleThread(LPVOID pCompPort) {
 					}
 				}
 				cout << "현재 접속 인원 수 : " << userMap.size() << endl;
-				// ReleaseMutex(mutex);
+				ReleaseMutex(mutex);
 				closesocket(sock);
-				free(handleInfo);
-				free(ioInfo);
+				delete handleInfo;
+				delete ioInfo;
 				continue;
 			}
 
-			packet = (P_PACKET_DATA) malloc(sizeof(PACKET_DATA));
+			packet = new PACKET_DATA;
 			memcpy(packet, ioInfo->buffer, sizeof(PACKET_DATA));
-			strcpy(msg, packet->name);
-			strcpy(mode, packet->message);
+			strcpy(name, packet->name);
+			strcpy(msg, packet->message);
 
-			// WaitForSingleObject(mutex, INFINITE);
+			int nowStatus = ((userMap.find(name))->second)->status;
+			direction = packet->direction;
+			cout << "nowStatus : " << nowStatus << endl;
+			cout << "msg : " << msg << endl;
 
-			cout << "received message : " << msg << endl;
-			cout << "clientMode : " << mode << endl;
-			free(ioInfo);
+			if (nowStatus == STATUS_WAITING) { // 대기실 케이스
+				if (direction == ROOM_MAKE) { // 새로운 방 만들때
+					cout << "ROOM_MAKE" << endl;
 
-			SendToAllMsg(ioInfo, msg, sock);
-			ioInfo = (LPPER_IO_DATA) malloc(sizeof(PER_IO_DATA));
+					// 유효성 검증 먼저
+					if (roomMap.count(msg) != 0) { // 방이름 중복
+						strcpy(msg, "이미 있는 방 이름입니다!\n");
+						strcat(msg, "1.방 정보 보기, 2.방 만들기 3.방 입장하기");
+						SendToMeMsg(ioInfo, msg, sock, STATUS_WAITING);
+						// 중복 케이스는 방 만들 수 없음
+					} else { // 방이름 중복 아닐 때만 개설
+
+						WaitForSingleObject(mutex, INFINITE);
+						// 새로운 방 정보 저장
+						list<string> chatList;
+						chatList.push_back(name);
+						roomMap.insert(
+								pair<string, list<string> >(msg, chatList));
+						// User의 상태 정보 바꾼다
+						strcpy(userMap.find(name)->second->roomName, msg);
+						userMap.find(name)->second->status = STATUS_CHATTIG;
+
+						ReleaseMutex(mutex);
+						strcat(msg, " 방이 개설되었습니다");
+						cout << msg << endl;
+						cout << "현재 서버의 방 갯수 : " << roomMap.size() << endl;
+						SendToMeMsg(ioInfo, msg, sock, STATUS_CHATTIG);
+					}
+
+				} else if (direction == ROOM_ENTER) { // 방 입장 요청
+					cout << "ROOM_ENTER" << endl;
+
+					// 유효성 검증 먼저
+					if (roomMap.count(msg) == 0) { // 방 못찾음
+						strcpy(msg, "없는 방 입니다!\n");
+						strcat(msg, "1.방 정보 보기, 2.방 만들기 3.방 입장하기");
+						SendToMeMsg(ioInfo, msg, sock, STATUS_WAITING);
+					} else {
+						WaitForSingleObject(mutex, INFINITE);
+
+						roomMap.find(msg)->second.push_back(name);
+						strcpy(userMap.find(name)->second->roomName, msg);
+						userMap.find(name)->second->status = STATUS_CHATTIG;
+
+						ReleaseMutex(mutex);
+
+						char sendMsg[NAME_SIZE + BUF_SIZE + 4];
+						strcpy(sendMsg, name);
+						strcat(sendMsg, " 님이 입장하셨었습니다");
+						cout << sendMsg << endl;
+						SendToRoomMsg(ioInfo, sendMsg,
+								roomMap.find(msg)->second,
+								STATUS_CHATTIG);
+					}
+				} else if (strcmp(msg, "1") == 0) { // 방 정보 요청시
+
+					WaitForSingleObject(mutex, INFINITE);
+					map<string, list<string> >::iterator iter;
+					char str[BUF_SIZE];
+					if (roomMap.size() == 0) {
+						strcpy(str, "만들어진 방이 없습니다\n");
+					} else {
+						strcpy(str, "방 정보 리스트\n");
+						// 방정보를 문자열로 만든다
+						for (iter = roomMap.begin(); iter != roomMap.end();
+								iter++) {
+							char num[2];
+							strcat(str, (iter->first).c_str());
+							strcat(str, " : ");
+							sprintf(num, "%d", (iter->second).size());
+							strcat(str, num);
+							strcat(str, " 명 ");
+							strcat(str, "\n");
+						}
+					}
+					ReleaseMutex(mutex);
+					SendToMeMsg(ioInfo, str, sock, STATUS_WAITING);
+				}
+			} else if (nowStatus == STATUS_CHATTIG) { // 채팅 중 케이스
+
+				if (bytesTrans == 0 || strcmp(msg, "out") == 0
+						|| strcmp(msg, "Q") == 0 || strcmp(msg, "q") == 0) { // 채팅방 나감
+					cout << "채팅방 나가기" << endl;
+
+					char sendMsg[NAME_SIZE + BUF_SIZE + 4];
+					strcpy(sendMsg, name);
+					strcat(sendMsg, " 님이 나갔습니다!");
+					cout << sendMsg << endl;
+
+					SendToRoomMsg(ioInfo, sendMsg,
+							roomMap.find(userMap.find(name)->second->roomName)->second,
+							STATUS_CHATTIG);
+					char roomName[NAME_SIZE];
+					strcpy(roomName, userMap.find(name)->second->roomName);
+					// 방이름 임시 저장
+					strcpy(userMap.find(name)->second->roomName, "");
+					userMap.find(name)->second->status = STATUS_WAITING;
+					strcpy(msg, "1.방 정보 보기, 2.방 만들기 3.방 입장하기");
+					SendToMeMsg(ioInfo, msg, sock, STATUS_WAITING);
+					cout << "나가는 사람 name : " << name << endl;
+
+					// 나가는 사람 정보 out
+					(roomMap.find(roomName)->second).remove(name);
+
+					if ((roomMap.find(roomName)->second).size() == 0) { // 방인원 0명이면 방 삭제
+						roomMap.erase(roomName);
+					}
+				} else { // 채팅방에서 채팅중
+					cout << "채팅방에 있는중 " << endl;
+					char sendMsg[NAME_SIZE + BUF_SIZE + 4];
+					strcpy(sendMsg, name);
+					strcat(sendMsg, " : ");
+					strcat(sendMsg, msg);
+					SendToRoomMsg(ioInfo, sendMsg,
+							roomMap.find(userMap.find(name)->second->roomName)->second,
+							STATUS_CHATTIG);
+				}
+
+			}
+
+			delete ioInfo;
+
+			ioInfo = new PER_IO_DATA;
 			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 			ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
 			ioInfo->wsaBuf.buf = ioInfo->buffer;
 			ioInfo->serverMode = READ;
 
+			// 계속 Recv
 			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags,
 					&(ioInfo->overlapped), NULL);
 		} else {
-			puts("message send");
-			//free(ioInfo);
+			cout << "message send" << endl;
+
 		}
 	}
 	return 0;
@@ -195,13 +406,18 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 
+	// Completion Port 생성
 	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 	GetSystemInfo(&sysInfo);
 
+	// CPU 갯수 만큼 스레드 생성
 	int process = sysInfo.dwNumberOfProcessors;
+	cout << "Server CPU num : " << process << endl;
 	for (i = 0; i < process; i++) {
 		_beginthreadex(NULL, 0, HandleThread, (LPVOID) hComPort, 0, NULL);
 	}
+	// Overlapped IO가능 소켓을 만든다
+	// TCP 통신할것
 	hServSock = WSASocketW(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, 0,
 	WSA_FLAG_OVERLAPPED);
 
@@ -220,9 +436,10 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 
+	// 임계영역 Object 생성
 	mutex = CreateMutex(NULL, FALSE, NULL);
 	cout << "Server ready listen" << endl;
-	cout << "port number" << argv[1] << endl;
+	cout << "port number : " << argv[1] << endl;
 
 	while (true) {
 		SOCKET hClientSock;
@@ -231,15 +448,17 @@ int main(int argc, char* argv[]) {
 		cout << "accept wait" << endl;
 		hClientSock = accept(hServSock, (SOCKADDR*) &clntAdr, &addrLen);
 
-		handleInfo = (LPPER_HANDLE_DATA) malloc(sizeof(PER_IO_DATA));
+		handleInfo = new PER_HANDLE_DATA;
 		handleInfo->hClntSock = hClientSock;
 		memcpy(&(handleInfo->clntAdr), &clntAdr, addrLen);
 
 		cout << "Connected client IP " << inet_ntoa(clntAdr.sin_addr) << endl;
 
+		// Completion Port 와 소켓 연결
 		CreateIoCompletionPort((HANDLE) hClientSock, hComPort,
 				(DWORD) handleInfo, 0);
-		ioInfo = (LPPER_IO_DATA) malloc(sizeof(PER_IO_DATA));
+
+		ioInfo = new PER_IO_DATA;
 		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 
 		ioInfo->wsaBuf.len = sizeof(PACKET_DATA);
@@ -249,8 +468,11 @@ int main(int argc, char* argv[]) {
 		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf), 1,
 				(LPDWORD) &recvBytes, (LPDWORD) &flags, &(ioInfo->overlapped),
 				NULL);
+
+		delete handleInfo;
 	}
 
+	// 임계영역 Object 반환
 	CloseHandle(mutex);
 	return 0;
 }
