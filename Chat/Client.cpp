c2c8e6d00eb2b33112309a6d823b9d4e4a9bb159
최인성 <choiis1207@gmail.com>
2019-01-06 21:17:56 +0900

    20180109 MultiChatRoom

--- Chat/Client.cpp
+++ Chat/Client.cpp
@@ -1,10 +1,11 @@
 //============================================================================
 // Name        : Client.cpp
-// Author      : 
+// Author      : Insung
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
