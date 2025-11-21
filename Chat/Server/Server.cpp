b0d15357cf5a34739c2c4ac9a3fde8e775850174
insung choi <choiis1207@gmail.com>
2025-11-21 12:28:27 +0900

    20251121 iocp server upgrade

--- Chat/Server/Server.cpp
+++ Chat/Server/Server.cpp
@@ -1,164 +1,247 @@
-﻿//============================================================================
-// Name        : Server.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-#include <iostream>
+﻿#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <process.h>
 #include <direct.h>
 #include <atomic>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <algorithm>
+
+#include <winsock2.h>
+#include <windows.h>
+
 #include "Socket.h"
 #include "BusinessService.h"
 
+// 필요 시 Ws2_32.lib 링크
+// #pragma comment(lib, "Ws2_32.lib")
+
 using namespace std;
+
+// ======================= 전역 객체 & 타입 정의 =======================
 
 // 내부 비지니스 로직 처리 클래스
 BusinessService::BusinessService businessService;
 
-// Recv가 Work에게 전달
+// 🔹 여기서 더 이상 JOB_DATA를 정의하지 않는다!
+//    -> 기존 프로젝트의 JOB_DATA 정의를 그대로 사용
+// struct JOB_DATA { ... }  <= 이거 전부 삭제
+
+// Recv -> Work 전달용 잡 큐
 queue<JOB_DATA> jobQueue;
 
+// 큐 보호용 mutex + condition_variable
 mutex queueCs;
+condition_variable jobCv;   // WorkThread 깨우기용
 
+// 패킷 카운터 (CALLCOUNT 용)
 atomic<int> packetCnt = 0;
 
-// Send broadcast 담당하는 스레드
-unsigned WINAPI SendThread(void *arg) {
+// Send 작업용 큐
+struct SEND_JOB {
+	SOCKET socket;
+	std::string msg;
+	ClientStatus status;
+};
 
-	while (true) {
-		businessService.Sendwork();
+// SendThread 처리용 send 큐 & 동기화 객체
+queue<SEND_JOB> sendQueue;
+mutex sendCs;
+condition_variable sendCv;
+
+// ======================= Send 큐 헬퍼 함수 =======================
+
+void EnqueueSend(const SEND_JOB& job) {
+	{
+		lock_guard<mutex> lock(sendCs);
+		sendQueue.push(job);
 	}
-	return 0;
+	sendCv.notify_one();
 }
 
+// ======================= SendThread =======================
 
-// Server 컴퓨터  CPU 개수만큼 스레드 생성될것
-// 내부 로직은 각 함수별로 처리
-unsigned WINAPI WorkThread(void *arg) {
+unsigned WINAPI SendThread(void* arg) {
+	UNREFERENCED_PARAMETER(arg);
 
 	while (true) {
+		queue<SEND_JOB> localQueue;
 
-		if (!jobQueue.empty()) {
+		{
+			unique_lock<mutex> lock(sendCs);
+			sendCv.wait(lock, [] {
+				return !sendQueue.empty();
+			});
 
-			queue<JOB_DATA> copyJobQueue;
-			{
-				lock_guard<mutex> guard(queueCs);  // Lock최소화
-				// 큐 통채 복사
-				copyJobQueue = jobQueue;
-				queue<JOB_DATA> emptyQueue; // 빈 큐
-				swap(jobQueue, emptyQueue); // 빈 큐로 바꿔치기
-			}
-			
-			while (!copyJobQueue.empty()) { // 여러 패킷 데이터 한꺼번에 처리
-				JOB_DATA jobData = copyJobQueue.front();
-				copyJobQueue.pop();
-
-				if (businessService.IsSocketDead(jobData.socket)) {
-					continue;
-				}
-
-				if (jobData.direction == Direction::CALLCOUNT) { // node js 서버로 전체 로그인된 유저수 
-
-					int cnt = packetCnt;
-					packetCnt = 0;
-					businessService.CallCnt(jobData.socket, cnt);
-				}
-				else if (jobData.direction == Direction::BAN) {
-					businessService.BanUser(jobData.socket, jobData.msg.substr(0, jobData.nowStatus).c_str());
-				}
-				else { 
-					ClientStatus status = businessService.GetStatus(
-						jobData.socket);
-
-					businessService.callFuncPointer(jobData.socket, status, jobData.direction, jobData.msg.c_str());
-				}
-			}
-		}
-		else {  // 동작안할  다른 스레드가 일할 수 있게
-			Sleep(1);
+			sendQueue.swap(localQueue);
 		}
 
+		while (!localQueue.empty()) {
+			SEND_JOB job = std::move(localQueue.front());
+			localQueue.pop();
+
+			if (businessService.IsSocketDead(job.socket)) {
+				continue;
+			}
+
+			businessService.getIocpService()->SendToOneMsg(
+				job.msg.c_str(),
+				job.socket,
+				job.status
+			);
+		}
 	}
 
 	return 0;
 }
 
-// Server 컴퓨터  CPU 개수만큼 스레드 생성될것
-// 내부 로직은 각 함수별로 처리
+// ======================= WorkThread =======================
+
+unsigned WINAPI WorkThread(void* arg) {
+	UNREFERENCED_PARAMETER(arg);
+
+	while (true) {
+		queue<JOB_DATA> localQueue;
+
+		{
+			unique_lock<mutex> lock(queueCs);
+
+			jobCv.wait(lock, [] {
+				return !jobQueue.empty();
+			});
+
+			jobQueue.swap(localQueue);
+		}
+
+		while (!localQueue.empty()) {
+			JOB_DATA jobData = std::move(localQueue.front());
+			localQueue.pop();
+
+			if (businessService.IsSocketDead(jobData.socket)) {
+				continue;
+			}
+
+			if (jobData.direction == Direction::CALLCOUNT) {
+				int cnt = packetCnt.exchange(0);
+				businessService.CallCnt(jobData.socket, cnt);
+			}
+			else if (jobData.direction == Direction::BAN) {
+				// 🔹 nowStatus가 ClientStatus(enum)일 가능성이 높으므로 캐스팅
+				businessService.BanUser(
+					jobData.socket,
+					jobData.msg.substr(
+						0,
+						static_cast<int>(jobData.nowStatus)
+					).c_str()
+				);
+			}
+			else {
+				ClientStatus status = businessService.GetStatus(jobData.socket);
+
+				businessService.callFuncPointer(
+					jobData.socket,
+					status,
+					jobData.direction,
+					jobData.msg.c_str()
+				);
+			}
+		}
+	}
+
+	return 0;
+}
+
+// ======================= RecvThread =======================
+
 unsigned WINAPI RecvThread(LPVOID pCompPort) {
-
-	// Completion port객체
 	HANDLE hComPort = (HANDLE)pCompPort;
-	SOCKET sock;
-	short bytesTrans;
-	LPPER_IO_DATA ioInfo;
+	SOCKET sock = INVALID_SOCKET;
+	DWORD bytesTrans = 0;
+	LPPER_IO_DATA ioInfo = nullptr;
 
 	while (true) {
+		BOOL success = GetQueuedCompletionStatus(
+			hComPort,
+			&bytesTrans,
+			(PULONG_PTR)& sock,
+			(LPOVERLAPPED*)& ioInfo,
+			INFINITE
+		);
 
-		bool success = GetQueuedCompletionStatus(hComPort, (LPDWORD)&bytesTrans,
-			(PULONG_PTR)&sock, (LPOVERLAPPED*)&ioInfo, INFINITE);
+		if (!success) {
+			DWORD errorNum = WSAGetLastError();
 
-		if (bytesTrans == 0 && !success) { // 접속 끊김 콘솔 강제 종료
+			if (ioInfo == nullptr) {
+				cout << "GetQueuedCompletionStatus failed, ioInfo == nullptr, error: " << errorNum << endl;
+				continue;
+			}
 
-			int errorNum = WSAGetLastError();
-			switch (errorNum)
-			{
-			case ERROR_IO_PENDING: // 대기열이 혼잡한 경우
+			switch (errorNum) {
+			case ERROR_IO_PENDING:
 				cout << "ERROR_IO_PENDING " << endl;
 				break;
-			case ERROR_NETNAME_DELETED: // ungraceful close
+			case ERROR_NETNAME_DELETED:
 				cout << "ERROR_NETNAME_DELETED " << endl;
 				break;
-			case ERROR_SEM_TIMEOUT: // 네트워크단절현상으로 timeout
+			case ERROR_SEM_TIMEOUT:
 				cout << "ERROR_SEM_TIMEOUT " << endl;
 				break;
-			case ERROR_OPERATION_ABORTED: // socket close Event
+			case ERROR_OPERATION_ABORTED:
 				cout << "ERROR_OPERATION_ABORTED " << endl;
 				break;
 			default:
-				cout << "else " << endl;
+				cout << "GetQueuedCompletionStatus error: " << errorNum << endl;
 				break;
 			}
 
 			businessService.ClientExit(sock);
-			MPool* mp = MPool::getInstance();
-			mp->Free(ioInfo);
+			MPool::getInstance()->Free(ioInfo);
+			continue;
 		}
-		else if (businessService.getIocpService()->RECV == ioInfo->serverMode
-			|| businessService.getIocpService()->RECV_MORE == ioInfo->serverMode) { // Recv 가 기본 동작
-			// 데이터 읽기 과정
-			short remainByte = min(bytesTrans, BUF_SIZE); // 초기 Remain Byte
+
+		// 🔹 success == TRUE && bytesTrans == 0 → graceful disconnect 처리
+		if (bytesTrans == 0) {
+			cout << "Client graceful disconnect" << endl;
+
+			businessService.ClientExit(sock);
+			if (ioInfo != nullptr) {
+				MPool::getInstance()->Free(ioInfo);
+			}
+			continue;
+		}
+
+		if (businessService.getIocpService()->RECV == ioInfo->serverMode
+			|| businessService.getIocpService()->RECV_MORE == ioInfo->serverMode) {
+
+			short remainByte = static_cast<short>(min<DWORD>(bytesTrans, BUF_SIZE));
 			bool recvMore = false;
 
-			// jobQueue에 데이터를 한번에 담기위한 자료구조
 			queue<JOB_DATA> packetQueue;
 
 			while (true) {
 				remainByte = businessService.PacketReading(ioInfo, remainByte);
-				// 다 받은 후 정상 로직
-				// DataCopy내에서 사용 메모리 전부 반환
+
 				if (remainByte >= 0) {
 					JOB_DATA jobData;
-					jobData.msg = businessService.DataCopy(ioInfo, &jobData.nowStatus,
-						&jobData.direction);
+					// 🔹 여기서 nowStatus 타입은 원래 정의 그대로 (ClientStatus일 것)
+					jobData.msg = businessService.DataCopy(
+						ioInfo,
+						&jobData.nowStatus,
+						&jobData.direction
+					);
 					jobData.socket = sock;
-					packetQueue.push(jobData);
-					// packetQueue 채움
+					packetQueue.push(std::move(jobData));
 				}
 
 				if (remainByte == 0) {
-					MPool* mp = MPool::getInstance();
-					mp->Free(ioInfo);
+					MPool::getInstance()->Free(ioInfo);
 					break;
 				}
-				else if (remainByte < 0) { // 받은 패킷 부족 || 헤더 다 못받음 -> 더받아야함
-					businessService.getIocpService()->RecvMore(
-						sock, ioInfo); // 패킷 더받기 & 기본 ioInfo 보존
-
+				else if (remainByte < 0) {
+					businessService.getIocpService()->RecvMore(sock, ioInfo);
 					recvMore = true;
 					break;
 				}
@@ -166,22 +249,25 @@ unsigned WINAPI RecvThread(LPVOID pCompPort) {
 
 			{
 				lock_guard<mutex> guard(queueCs);
-				packetCnt.fetch_add(static_cast<int>(packetQueue.size()));
-				while (!packetQueue.empty()) { // packetQueue -> jobQueue 
-					JOB_DATA jobData = packetQueue.front();
+
+				packetCnt.fetch_add(
+					static_cast<int>(packetQueue.size()),
+					std::memory_order_relaxed
+				);
+
+				while (!packetQueue.empty()) {
+					JOB_DATA jobData = std::move(packetQueue.front());
 					packetQueue.pop();
-					jobQueue.push(jobData);
+					jobQueue.push(std::move(jobData));
 				}
 			}
-			// jobQueue에 한번에 Insert
+			jobCv.notify_all();
 
-			if (!recvMore) { // recvMore이 아니면 해당 socket은 받기 동작을 계속한다
-				businessService.getIocpService()->Recv(
-					sock); // 패킷 더받기
+			if (!recvMore) {
+				businessService.getIocpService()->Recv(sock);
 			}
 		}
-		else if (businessService.getIocpService()->SEND == ioInfo->serverMode) { // Send 끝난경우
-
+		else if (businessService.getIocpService()->SEND == ioInfo->serverMode) {
 			CharPool* charPool = CharPool::getInstance();
 
 			charPool->Free(ioInfo->wsaBuf.buf);
@@ -190,17 +276,19 @@ unsigned WINAPI RecvThread(LPVOID pCompPort) {
 			mp->Free(ioInfo);
 		}
 		else {
-
 			MPool* mp = MPool::getInstance();
 			mp->Free(ioInfo);
 		}
-		
 	}
+
 	return 0;
 }
 
+// ======================= main =======================
 
 int main(int argc, char* argv[]) {
+	UNREFERENCED_PARAMETER(argc);
+	UNREFERENCED_PARAMETER(argv);
 
 	HANDLE hComPort;
 	SYSTEM_INFO sysInfo;
@@ -210,31 +298,29 @@ int main(int argc, char* argv[]) {
 	SetConsoleTextAttribute(
 		GetStdHandle(STD_OUTPUT_HANDLE), 10);
 
-	// Completion Port 생성
 	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 	GetSystemInfo(&sysInfo);
 
 	cout << "Server ready listen" << endl;
 	cout << "port number : " << SERVER_PORT << endl;
 
-	int process = sysInfo.dwNumberOfProcessors;
+	int process = static_cast<int>(sysInfo.dwNumberOfProcessors);
 	cout << "Server CPU num : " << process << endl;
-	Dao::GetInstance(); // SQL Server Connection 초기화 지점
-	MPool* mp = MPool::getInstance(); // 메모리풀 초기화 지점
-	CharPool* charPool = CharPool::getInstance(); // 메모리풀 초기화 지점
 
-	// Thread Pool Client에게 패킷 받는 동작
+	Dao::GetInstance();
+	MPool* mp = MPool::getInstance();
+	CharPool* charPool = CharPool::getInstance();
+	(void)mp;
+	(void)charPool;
+
 	for (int i = 0; i < process; i++) {
-		// 만들어진 HandleThread를 hComPort CP 오브젝트에 할당한다
 		_beginthreadex(NULL, 0, RecvThread, (LPVOID)hComPort, 0, NULL);
 	}
 
-	// Thread Pool 비지니스 로직 담당
 	for (int i = 0; i < 2 * process; i++) {
 		_beginthreadex(NULL, 0, WorkThread, NULL, 0, NULL);
 	}
 
-	// Thread Pool BroadCast 해줌
 	for (int i = 0; i < (process * 5) / 3; i++) {
 		_beginthreadex(NULL, 0, SendThread, NULL, 0, NULL);
 	}
@@ -243,23 +329,34 @@ int main(int argc, char* argv[]) {
 		SOCKET hClientSock;
 		SOCKADDR_IN clntAdr;
 		int addrLen = sizeof(clntAdr);
-		hClientSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
+		hClientSock = accept(hServSock, (SOCKADDR*)& clntAdr, &addrLen);
+
+		if (hClientSock == INVALID_SOCKET) {
+			int err = WSAGetLastError();
+			cout << "accept failed, error: " << err << endl;
+			continue;
+		}
 
 		businessService.InsertLiveSocket(hClientSock, clntAdr);
 
-		// Completion Port 와 accept한 소켓 연결
-		CreateIoCompletionPort((HANDLE)hClientSock, hComPort,
-			(DWORD)hClientSock, 0);
+		CreateIoCompletionPort(
+			(HANDLE)hClientSock,
+			hComPort,
+			(ULONG_PTR)hClientSock,
+			0
+		);
 
 		businessService.getIocpService()->Recv(hClientSock);
 
-		// 초기 접속 메세지 Send
 		string str = "접속을 환영합니다!\n";
 		str += loginBeforeMessage;
 
 		businessService.getIocpService()->SendToOneMsg(
-			str.c_str(), hClientSock, ClientStatus::STATUS_LOGOUT);
+			str.c_str(),
+			hClientSock,
+			ClientStatus::STATUS_LOGOUT
+		);
 	}
-	
+
 	return 0;
 }
